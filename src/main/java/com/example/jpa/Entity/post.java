f4597ee52ed796b03193e5d149a8e47729e79150
syyu21b <syyu21b@gmail.com>
2026-06-05 10:19:19 +0900

    jpa 학습용 프로젝트 생성

---
+++ src/main/java/com/example/jpa/Entity/post.java
@@ -0,0 +1,12 @@
+package com.example.jpa.Entity;
+
+import java.time.LocalDateTime;
+
+public class post {
+    private long id;
+    private String title;
+    private String content;
+    private String author;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
