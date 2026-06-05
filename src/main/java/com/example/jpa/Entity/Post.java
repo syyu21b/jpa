d50b8f46a988777ea4213106c3d085167f44982f
syyu21b <syyu21b@gmail.com>
2026-06-05 11:20:43 +0900

    Post 수정

--- src/main/java/com/example/jpa/Entity/Post.java
+++ src/main/java/com/example/jpa/Entity/Post.java
@@ -1,31 +1,41 @@
 package com.example.jpa.Entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "posts")
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Post {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    private Long id;
 
     @Column(nullable = false, length = 100)
     private String title;
 
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
+    @Column(nullable = false)
     private String author;
 
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
+    // 생성 메서드 (정적 팩토리 패턴)
     public static Post create(String title, String content, String author) {
         Post post = new Post();
         post.title = title;
@@ -34,6 +44,7 @@ public class Post {
         return post;
     }
 
+    // 수정 메서드
     public void update(String title, String content) {
         this.title = title;
         this.content = content;
